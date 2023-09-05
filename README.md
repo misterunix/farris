61a7bc96f49aa93648fd421d6fc70bbe0740b9e2
Bill Jones <misterunix@gmail.com>
2023-09-05 13:17:07 -0500

    push to get read to use go image

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+# farris
+
+
+github.com/StephaneBunel/bresenham
+github.com/golang/freetype
+github.com/x/image/font
+
 https://www.quantamagazine.org/how-to-create-art-with-mathematics-20151008/
+
+
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 
 	gd "github.com/misterunix/cgo-gd"
 	"github.com/misterunix/colorworks/hsl"
@@ -15,11 +16,11 @@ func main() {
 	width := 512
 	height := 512
 
-	v1 := 3.0
-	v2 := 13.0
+	v1 := -5.0
+	v2 := 21.0
 
-	is := "(0 to 2pi)"
-	formulaX := fmt.Sprintf("math.Cos(i) + math.Cos(%f*%s)/2.0 + math.Sin(%f*%s)/3.0", v1, is, v2, is)
+	//is := "(0 to 2pi)"
+	//formulaX := fmt.Sprintf("math.Cos(i) + math.Cos(%f*%s)/2.0 + math.Sin(%f*%s)/3.0", v1, is, v2, is)
 	//formulaY := fmt.Sprintf("math.Sin(i) + math.Sin(%f*%s)/2.0 + math.Cos(%f*%s)/3.0", v1, is, v2, is)
 
 	w1 := float64(width) / 2.0
@@ -30,17 +31,20 @@ func main() {
 	img := gd.CreateTrueColor(int(width), int(height))
 	defer img.Destroy()
 
-	tc := img.ColorAllocate(0, 0, 255)
-	check := img.StringFT(tc, "CallingCode-Regular.ttf", 18, 0, 10, 20, formulaX)
-	fmt.Println(check)
-
+	//tc := img.ColorAllocate(0, 0, 255)
+	//check := img.StringFT(tc, "CallingCode-Regular.ttf", 18, 0, 10, 20, formulaX)
+	//fmt.Println(check)
+	hueOffset := rand.Float64() * 360.0
 	img.Fill(0, 0, img.ColorAllocate(255, 255, 255))
 
 	for i := 0.0; i < 2.0*math.Pi; i += 0.00001 {
 		x := math.Cos(i) + math.Cos(v1*i)/2.0 + math.Sin(v2*i)/3.0
 		y := math.Sin(i) + math.Sin(v1*i)/2.0 + math.Cos(v2*i)/3.0
 
-		r, g, b := hsl.HSLtoRGB(i*57.295779513, 1.0, 0.5)
+		hue := math.Mod(i*57.295779513+hueOffset, 360) // 0 to 360
+
+		r, g, b := hsl.HSLtoRGB(hue, 1.0, 0.5)
+
 		c := img.ColorAllocate(int(r), int(g), int(b))
 		img.SetPixel(int(x*w2+w1), int(y*h2+h1), c)
 	}
